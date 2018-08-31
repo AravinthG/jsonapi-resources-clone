467abe7b65b856f841c10402bd912c989cac741b
aravinth <aravinthgk@gmail.com>
2018-08-31 16:32:28 +0530

    add id in attrs

--- lib/jsonapi/custom_resource_serializer.rb
+++ lib/jsonapi/custom_resource_serializer.rb
@@ -11,14 +11,12 @@ module JSONAPI
     def attributes_hash(source, fetchable_fields)
       fields = fetchable_fields & supplying_attribute_fields(source.class)
       fields.each_with_object({}) do |name, hash|
-        unless name == :id
-          format = source.class._attribute_options(name)[:format]
-          res = format_value(source.public_send(name), format)
-          if name == :additional_data || name == :all_attributes
-            hash.merge!(res)
-          else
-            hash[format_key(name)] = res
-          end
+        format = source.class._attribute_options(name)[:format]
+        res = format_value(source.public_send(name), format)
+        if name == :additional_data || name == :all_attributes
+          hash.merge!(res)
+        else
+          hash[format_key(name)] = res
         end
       end
     end
